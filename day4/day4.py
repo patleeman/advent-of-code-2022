464d8ef41e1e96aa92cb554a5c06368476df7c36
Patrick Lee <git@patricklee.nyc>
2023-03-09 21:16:03 -0500

    Finish day4

--- day4/day4.py
+++ day4/day4.py
@@ -1,7 +1,3 @@
-
-
-
-
 def part1(assignments):
     fully_covered = 0
     for assignment in assignments:
@@ -13,14 +9,25 @@ def part1(assignments):
         # a fully covers b
         if ea0 <= eb0 and ea1 >= eb1:
             fully_covered += 1
-            print('a', ea0, eb0, eb1, ea1)
         elif eb0 <= ea0 and eb1 >= ea1:
             fully_covered += 1
-            print('b', eb0, ea0, ea1, eb1)
 
-        
     return fully_covered
 
+
+def part2(assignments):
+    no_overlap = 0
+    for assignment in assignments:
+        elf_a, elf_b = [a.split('-') for a in assignment.split(',')]
+        ea0 = int(elf_a[0])
+        ea1 = int(elf_a[1])
+        eb0 = int(elf_b[0])
+        eb1 = int(elf_b[1])
+        # a fully covers b
+        if ea1 < eb0 or ea0 > eb1:
+            no_overlap += 1
+    print(no_overlap)
+    return len(assignments) - no_overlap
 
 
 if __name__ == "__main__":
@@ -29,3 +36,4 @@ if __name__ == "__main__":
     data = data.split('\n')
     del data[-1]
     print(part1(data))
+    print(part2(data))
